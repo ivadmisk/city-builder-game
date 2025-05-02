50ec5d09c8a4b8616d446dc80054c403523b751e
ivadmisk <ivadivan1@gmail.com>
2025-05-02 13:46:46 +0500

    Исправлены ошибки TypeScript и конфигурации

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -19,5 +18,6 @@ export default defineConfig({
   },
   build: {
     outDir: '../../dist/client',
+    emptyOutDir: true
   },
 }); 
